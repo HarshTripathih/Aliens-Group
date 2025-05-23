f64f491b54b0a51e3d4f0a5a9cc5eba17ad080a8
HarshTripathih <harshtripathih321@gmail.com>
2025-05-23 21:10:28 +0530

    done resonsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function HomePage() {
       <FeaturedCategories />
       <AwardsPage />
       <TestimonialsSection />
-      {/* <ClientHomeModal />  */}
+      <ClientHomeModal /> 
     </main>
   );
 }
